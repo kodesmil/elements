46bb366f574ba4ef2174b6971316d1ef7ff03f5f
Marcin Urbanski <marcinn.urbanski@gmail.com>
2024-09-29 11:32:33 -0700

    Update header

--- src/molecules/ks-header.tsx
+++ src/molecules/ks-header.tsx
@@ -1,12 +1,19 @@
 import { KsDropdownMenu } from '@covision/elements/molecules/dashboard/ks-dropdown-menu'
 import { KsSheet } from '@covision/elements/molecules/ks-sheet'
 import type { ReactNode } from 'react'
+import { cn } from '../lib'
 
 export function KsHeader(props: {
   action: ReactNode
+  className?: string
 }) {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center justify-between gap-4 border-b bg-background px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header
+      className={cn(
+        'sticky top-0 z-30 flex h-14 items-center justify-between gap-4 border-b px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6',
+        props.className
+      )}
+    >
       <div className={'flex gap-2'}>
         <KsSheet />
       </div>
