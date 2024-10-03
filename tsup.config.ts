45b0df226a8bd858551e0a99a2d5e7f3d1e335bf
Marcin Urbanski <marcinn.urbanski@gmail.com>
2024-10-02 18:41:48 -0700

    Add text editor without use client

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,4 @@
 import childProcess from 'node:child_process'
-import fs from 'node:fs'
-import { readFile } from 'node:fs/promises'
-import path from 'node:path'
 import { type Options, defineConfig } from 'tsup'
 
 const nextJsOptions: Options = {
@@ -25,43 +22,15 @@ const nextJsOptions: Options = {
   },
 }
 
-const _libsOptions: Options = {
-  entry: ['src/lib/index.ts'],
+const nextSsrJsOptions: Options = {
+  entry: ['src/molecules/ssr/index.ts'],
   sourcemap: 'inline',
   minify: true,
   clean: true,
   dts: true,
-  treeshake: true,
+  treeshake: false,
   splitting: true,
   format: ['esm'],
-}
-
-const getPackageName = async () => {
-  try {
-    const packageJson = JSON.parse(
-      await readFile(path.join(__dirname, 'package.json'), 'utf-8')
-    )
-    return packageJson.name
-  } catch (_error) {
-    return 'package-name'
-  }
-}
-
-const _addUseStatement = async (
-  basePath: string,
-  type: 'server' | 'client'
-) => {
-  const fullPath = path.join(__dirname, basePath)
-  const files = fs.readdirSync(fullPath)
-
-  for (const file of files) {
-    if (file.endsWith('.js') || file.endsWith('.mjs')) {
-      const filePath = path.join(fullPath, file)
-      let content = await readFile(filePath, 'utf-8')
-      content = `"use ${type}";\n${content}`
-      fs.writeFileSync(filePath, content, 'utf-8')
-    }
-  }
 }
 
 const linkSelf = async () => {
@@ -77,14 +46,11 @@ const linkSelf = async () => {
 }
 
 export default defineConfig([
-  // libsOptions,
   {
     async onSuccess() {
-      // If you want need to add a use statement to files, you can use the following code:
-      // await _addUseStatement('dist/react', 'client');
-
       await linkSelf()
     },
     ...nextJsOptions,
+    ...nextSsrJsOptions,
   },
 ])
