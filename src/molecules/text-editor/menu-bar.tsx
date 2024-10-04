dc20d2687d71578f9f25a1d8037d0af2d5a097aa
Marcin Urbanski <marcinn.urbanski@gmail.com>
2024-10-03 20:53:31 -0700

    Menu Bar

--- src/molecules/text-editor/menu-bar.tsx
+++ src/molecules/text-editor/menu-bar.tsx
@@ -1,197 +1,231 @@
+import { Button, type ButtonProps } from '@covision/elements/atoms'
+import { KsTooltip } from '@covision/elements/molecules'
+import { ListBulletIcon } from '@radix-ui/react-icons'
 import { useCurrentEditor } from '@tiptap/react'
+import {
+  AlignStartHorizontalIcon,
+  BoldIcon,
+  Code2Icon,
+  CodeIcon,
+  EraserIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon,
+  HighlighterIcon,
+  ItalicIcon,
+  ListOrderedIcon,
+  QuoteIcon,
+  RedoIcon,
+  RemoveFormattingIcon,
+  StrikethroughIcon,
+  TextCursorIcon,
+  UndoIcon,
+  WrapTextIcon,
+} from 'lucide-react'
+
+const MenuButton = (
+  props: { isActive: boolean; tooltip: string } & ButtonProps
+) => (
+  <KsTooltip tooltip={props.tooltip}>
+    <Button
+      size={'sm'}
+      type={'button'}
+      variant={props.isActive ? 'secondary' : 'ghost'}
+      className={'h-8 w-8 rounded-none border border-0 p-1.5'}
+      {...props}
+    />
+  </KsTooltip>
+)
 
 export const MenuBar = () => {
   const { editor } = useCurrentEditor()
 
   if (!editor) {
-    return <div>Hello</div>
+    return null
   }
 
   return (
-    <div className="flex w-full flex-wrap">
-      <div className="flex flex-row space-x-2">
-        <button
-          type={'button'}
+    <div className="flex w-full flex-wrap space-x-2 pb-6">
+      <div className="flex flex-row">
+        <MenuButton
+          tooltip={'Bold'}
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editor.can().chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'is-active' : ''}
+          isActive={editor.isActive('bold')}
         >
-          Bold
-        </button>
-        <button
-          type={'button'}
+          <BoldIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Italic'}
           onClick={() => editor.chain().focus().toggleItalic().run()}
           disabled={!editor.can().chain().focus().toggleItalic().run()}
-          className={editor.isActive('italic') ? 'is-active' : ''}
+          isActive={editor.isActive('italic')}
         >
-          Italic
-        </button>
-        <button
-          type={'button'}
+          <ItalicIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Strike'}
           onClick={() => editor.chain().focus().toggleStrike().run()}
           disabled={!editor.can().chain().focus().toggleStrike().run()}
-          className={editor.isActive('strike') ? 'is-active' : ''}
+          isActive={editor.isActive('strike')}
         >
-          Strike
-        </button>
-        <button
-          type={'button'}
+          <StrikethroughIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Code'}
           onClick={() => editor.chain().focus().toggleCode().run()}
           disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={editor.isActive('code') ? 'is-active' : ''}
+          isActive={editor.isActive('code')}
         >
-          Code
-        </button>
-        <button
-          type={'button'}
+          <CodeIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Clear'}
           onClick={() => editor.chain().focus().unsetAllMarks().run()}
+          isActive={false}
         >
-          Clear marks
-        </button>
-        <button
-          type={'button'}
+          <EraserIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Remove Formatting'}
+          isActive={false}
           onClick={() => editor.chain().focus().clearNodes().run()}
         >
-          Clear nodes
-        </button>
-        <button
-          type={'button'}
+          <RemoveFormattingIcon />
+        </MenuButton>
+      </div>
+      <div className="flex flex-row rounded">
+        <MenuButton
+          tooltip={'Paragraph'}
           onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editor.isActive('paragraph') ? 'is-active' : ''}
+          isActive={editor.isActive('paragraph')}
         >
-          Paragraph
-        </button>
-        <button
-          type={'button'}
+          <TextCursorIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 1'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 1 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 1 })}
         >
-          H1
-        </button>
-        <button
-          type={'button'}
+          <Heading1Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 2'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 2 })}
         >
-          H2
-        </button>
-        <button
-          type={'button'}
+          <Heading2Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 3'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 3 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 3 })}
         >
-          H3
-        </button>
-        <button
-          type={'button'}
+          <Heading3Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 4'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 4 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 4 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 4 })}
         >
-          H4
-        </button>
-        <button
-          type={'button'}
+          <Heading4Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 5'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 5 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 5 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 5 })}
         >
-          H5
-        </button>
-        <button
-          type={'button'}
+          <Heading5Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Heading 6'}
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 6 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 6 }) ? 'is-active' : ''
-          }
+          isActive={editor.isActive('heading', { level: 6 })}
         >
-          H6
-        </button>
-        <button
-          type={'button'}
+          <Heading6Icon />
+        </MenuButton>
+      </div>
+      <div className="flex flex-row rounded">
+        <MenuButton
+          tooltip={'Bullet List'}
           onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive('bulletList') ? 'is-active' : ''}
+          isActive={editor.isActive('bulletList')}
         >
-          Bullet list
-        </button>
-        <button
-          type={'button'}
+          <ListBulletIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Ordered List'}
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive('orderedList') ? 'is-active' : ''}
+          isActive={editor.isActive('orderedList')}
         >
-          Ordered list
-        </button>
-        <button
-          type={'button'}
+          <ListOrderedIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Code Block'}
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={editor.isActive('codeBlock') ? 'is-active' : ''}
+          isActive={editor.isActive('codeBlock')}
         >
-          Code block
-        </button>
-        <button
-          type={'button'}
+          <Code2Icon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Blockquote'}
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editor.isActive('blockquote') ? 'is-active' : ''}
+          isActive={editor.isActive('blockquote')}
         >
-          Blockquote
-        </button>
-        <button
-          type={'button'}
+          <QuoteIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Horizontal Rule'}
+          isActive={false}
           onClick={() => editor.chain().focus().setHorizontalRule().run()}
         >
-          Horizontal rule
-        </button>
-        <button
-          type={'button'}
+          <AlignStartHorizontalIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Wrap Text'}
+          isActive={false}
           onClick={() => editor.chain().focus().setHardBreak().run()}
         >
-          Hard break
-        </button>
-        <button
-          type={'button'}
+          <WrapTextIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Undo'}
+          isActive={false}
           onClick={() => editor.chain().focus().undo().run()}
           disabled={!editor.can().chain().focus().undo().run()}
         >
-          Undo
-        </button>
-        <button
-          type={'button'}
+          <UndoIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Redo'}
+          isActive={false}
           onClick={() => editor.chain().focus().redo().run()}
           disabled={!editor.can().chain().focus().redo().run()}
         >
-          Redo
-        </button>
-        <button
-          type={'button'}
+          <RedoIcon />
+        </MenuButton>
+        <MenuButton
+          tooltip={'Highlight'}
           onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-          className={
-            editor.isActive('textStyle', { color: '#958DF1' })
-              ? 'is-active'
-              : ''
-          }
+          isActive={editor.isActive('textStyle', { color: '#958DF1' })}
         >
-          Purple
-        </button>
+          <HighlighterIcon />
+        </MenuButton>
       </div>
     </div>
   )
--- src/molecules/text-editor/text-editor.tsx
+++ src/molecules/text-editor/text-editor.tsx
@@ -1,6 +1,7 @@
 import { defaultExtensions } from '@covision/elements/molecules'
 import { MenuBar } from '@covision/elements/molecules/text-editor/menu-bar'
 import { EditorProvider } from '@tiptap/react'
+import { cx } from 'class-variance-authority'
 
 interface EditorProp {
   defaultValue?: string
@@ -9,11 +10,21 @@ interface EditorProp {
 
 export const KsTextEditor = ({ defaultValue, onChange }: EditorProp) => {
   return (
-    <div className="prose prose-lg dark:prose-invert w-full font-serif prose-headings:font-title focus:outline-none">
+    <div
+      className={cx(
+        'ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'prose prose-lg dark:prose-invert max-w-full rounded-md border border-input bg-background px-3 py-2 font-serif prose-headings:font-title text-foreground'
+      )}
+    >
       <EditorProvider
         slotBefore={<MenuBar />}
         content={defaultValue}
         extensions={defaultExtensions}
+        editorProps={{
+          attributes: {
+            class: 'focus:outline-none',
+          },
+        }}
         onUpdate={({ editor }) => {
           onChange(editor.getHTML())
         }}
