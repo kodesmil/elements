99fcb0784ac408c2175f76df7267fb0f0b377ee4
Marcin Urbanski <marcinn.urbanski@gmail.com>
2024-10-08 17:47:08 -0700

    Update text size

--- src/atoms/shacdn/ui/input.tsx
+++ src/atoms/shacdn/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-input-border bg-input-background px-3 py-2 text-base text-foreground text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full rounded-md border border-input-border bg-input-background px-3 py-2 text-base text-foreground ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
